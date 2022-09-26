1d84b6b4681a54493a772442266870848b26b4d9
Umayr <Umayrkaleem@gmail.com>
2022-09-26 15:07:44 -0500

    added readCSV functionality

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-import ()
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
 
-func main() {}
+type problem struct {
+	Question string
+	Answer   string
+}
+
+func main() {
+
+	readCSV()
+
+}
+
+func readCSV() {
+	csvFile, err := os.Open("problems.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened CSV file")
+	defer csvFile.Close()
+
+	csvLines, err := csv.NewReader(csvFile).ReadAll()
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, line := range csvLines {
+		prob := problem{
+			Question: line[0],
+			Answer:   line[1],
+		}
+		fmt.Println(prob.Question + " " + prob.Answer)
+	}
+}
